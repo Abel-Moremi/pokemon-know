28ce4cd16477f89db8d9c4fc16b6dd6c18adfbcd
Abel Moremi <abelmoremiz@gmail.com>
2021-05-11 18:06:00 +0200

    fix: remove unnecessary route

---
+++ pages/_slug.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        <pokemon :pokemon="pokemon"/>
+        <pokemon-stats :pokemon="pokemon" />
+        <pokemon-abilities :pokemon="pokemon" />
+    </div>
+</template>
+
+
+<script>
+import Pokemon from '~/components/Pokemon.vue'
+import PokemonAbilities from '~/components/PokemonAbilities.vue'
+import PokemonStats from '~/components/PokemonStats.vue'
+
+export default {
+  components: { 
+    Pokemon, 
+    PokemonStats, 
+    PokemonAbilities 
+  },
+  async asyncData({params, $axios}) {
+    const slug = params.slug
+    const poki = await $axios.$get('https://pokeapi.co/api/v2/pokemon/'+slug)
+    console.log(poki)
+    return { pokemon:poki }
+	}
+}
+</script>
