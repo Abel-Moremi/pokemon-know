0ab68520373e62f82561d80b73027cabf2ae8068
Abel-Moremi <Abel-Moremi@users.noreply.github.com>
2021-05-12 02:33:38 +0000

    deploy: 0f1baf88cd7706f53a20e05c7f6eb294fe9c020b

---
+++ _nuxt/static/1620786818/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpokemon-know\u002F_nuxt\u002Fstatic\u002F1620786818",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpokemon-know\u002F",assetsPath:"\u002Fpokemon-know\u002F_nuxt\u002F",cdnURL:a}}}}(null));
